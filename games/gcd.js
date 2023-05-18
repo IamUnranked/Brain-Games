356eb492014a043550404f882fe3f4d346fcabf9
Valentine Gorobtsov <valentingorobtsov@gmail.com>
2023-05-18 20:28:46 +0500

    step seven

---
+++ games/gcd.js
@@ -0,0 +1,26 @@
+import gameBase from '../src/index.js';
+import random from '../src/utils.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+const game = () => {
+  let numberOne = random(1, 10);
+  let numberTwo = random(1, 10);
+  const question = `${numberOne} ${numberTwo}`;
+  const gameCorrectAnswer = [];
+
+  while (numberOne !== 0 && numberTwo !== 0) {
+    if (numberOne > numberTwo) {
+      numberOne %= numberTwo;
+    } else {
+      numberTwo %= numberOne;
+    }
+  }
+  gameCorrectAnswer.push(`${numberOne + numberTwo}`);
+  const temp = gameCorrectAnswer.join('');
+  return [question, temp];
+};
+
+const gcd = () => gameBase(description, game);
+
+export default gcd;
